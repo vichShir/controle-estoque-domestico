48362105e46ab5519ca1416c56e8b9bf1ffe898e
vichShir <victor.shirasuna@gmail.com>
2022-02-11 15:40:36 -0300

    Atualizado Projeto SQL Versão 1.5

--- sql-project/3. Projeto Físico/ddl.sql
+++ sql-project/3. Projeto Físico/ddl.sql
@@ -16,26 +16,50 @@ CREATE TABLE local
 	PRIMARY KEY(codlocal)
 );
 
+CREATE TABLE categoria
+(
+	codcategoria TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(20) NOT NULL,
+	PRIMARY KEY(codcategoria)
+);
+
+CREATE TABLE unidademedida
+(
+	codmedida TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(16) NOT NULL,
+	PRIMARY KEY(codmedida)
+);
+
 CREATE TABLE item
 (
 	coditem INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	barcode CHAR(13) NULL,
 	nome VARCHAR(50) NOT NULL,
-	categoria VARCHAR(30) NULL,
-	unidademedida VARCHAR(10) NOT NULL,
 	quantidade DECIMAL(6,2) NOT NULL,
 	codlocal TINYINT UNSIGNED NOT NULL,
+	codcategoria TINYINT UNSIGNED NOT NULL,
+	codmedida TINYINT UNSIGNED NOT NULL,
 	PRIMARY KEY(coditem),
-	FOREIGN KEY(codlocal) REFERENCES local(codlocal)
+	FOREIGN KEY(codlocal) REFERENCES local(codlocal),
+	FOREIGN KEY(codcategoria) REFERENCES categoria(codcategoria),
+	FOREIGN KEY(codmedida) REFERENCES unidademedida(codmedida)
 );
 
 CREATE INDEX ix_item_local
 ON item(codlocal);
 
+CREATE INDEX ix_item_categoria
+ON item(codcategoria);
+
+CREATE INDEX ix_item_medida
+ON item(codmedida);
+
 SELECT * FROM usuario;
 SELECT * FROM local;
+SELECT * FROM categoria;
+SELECT * FROM unidademedida;
 SELECT * FROM item;
 
 /*
-DROP TABLE item, local, usuario;
+DROP TABLE item, unidademedida, categoria, local, usuario;
 */
--- sql-project/3. Projeto Físico/dml.sql
+++ sql-project/3. Projeto Físico/dml.sql
@@ -4,11 +4,19 @@
 INSERT INTO usuario(nome, username, senha) VALUES('Victor Shirasuna', 'vichShir', '5t6y7u8i');
 
 /* Local */
-INSERT INTO local(nome, sublocal) VALUES('Cozinha', 'Armazem');
+INSERT INTO local(nome, sublocal) VALUES('COZINHA', 'ARMAZEM');
+
+/* Categoria */
+INSERT INTO categoria(nome) VALUES('BEBIDAS');
+INSERT INTO categoria(nome) VALUES('ENLATADOS');
+
+/* Unidade Medida */
+INSERT INTO unidademedida(nome) VALUES('UNITARIO');
 
 /* Item */
-INSERT INTO item(barcode, nome, categoria, unidademedida, quantidade, codlocal) VALUES('7891103217975', 'AGUA DE COCO CAIXA 1L KERO COCO', 'BEBIDAS', 'UNITARIO', 6, 1);
-INSERT INTO item(barcode, nome, categoria, unidademedida, quantidade, codlocal) VALUES('1112223334445', 'AGUA COM GAS CRYSTAL 1L', 'BEBIDAS', 'UNITARIO', 3, 1);
+INSERT INTO item(barcode, nome, quantidade, codlocal, codcategoria, codmedida) VALUES('7891103217975', 'AGUA DE COCO CAIXA 1L KERO COCO', 6, 1, 1, 1);
+INSERT INTO item(barcode, nome, quantidade, codlocal, codcategoria, codmedida) VALUES('1112223334445', 'AGUA COM GAS CRYSTAL 1L', 3, 1, 1, 1);
+INSERT INTO item(barcode, nome, quantidade, codlocal, codcategoria, codmedida) VALUES('1112223334446', 'ATUM ENTALADO', 2, 1, 2, 1);
 
 
 /*** VIEWS ***/
@@ -16,9 +24,13 @@ INSERT INTO item(barcode, nome, categoria, unidademedida, quantidade, codlocal) 
 /* Produtos */
 CREATE VIEW VW_PRODUTOS
 AS
-SELECT l.nome LOCAL, l.sublocal SUBLOCAL, i.categoria CATEGORIA, i.barcode BARCODE, i.nome NOME, i.unidademedida UNIDADE_MEDIDA, i.quantidade QUANTIDADE
-	FROM local l INNER JOIN item i
-		ON l.codlocal = i.codlocal;
+SELECT l.nome LOCAL, l.sublocal SUBLOCAL, c.nome CATEGORIA, i.barcode BARCODE, i.nome NOME, u.nome UNIDADE_MEDIDA, i.quantidade QUANTIDADE
+	FROM item i INNER JOIN local l
+		ON i.codlocal = l.codlocal
+		INNER JOIN categoria c
+		ON i.codcategoria = c.codcategoria
+		INNER JOIN unidademedida u
+		ON i.codmedida = u.codmedida;
 
 /* Usuarios */
 CREATE VIEW VW_USERS
